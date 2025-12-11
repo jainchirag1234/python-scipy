b936fdcb87802bd9aba5f00313985ee3fd6a726c
jainchirag1234 <141317118+jainchirag1234@users.noreply.github.com>
2025-12-11 09:17:21 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+import scipy as sp
+from scipy import special
+
+func1=special.sindg(30)
+func2=special.sindg(6)
+func3=special.sindg(90)
+func4=special.cosdg(90)
+print(func1)
+print(func2)
+print(func3)
+print(func4)
